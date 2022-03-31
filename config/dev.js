f019fa29ca89003c2dd6f8d8e45bc560d03617ab
Rahul Choudhary <77048625+rahulc2303@users.noreply.github.com>
2022-03-31 19:39:56 +0530

    Create dev.js

---
+++ config/dev.js
@@ -0,0 +1,17 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://shopisthan:shopisthan@cluster0.iwbbb.mongodb.net/New-Flow?retryWrites=true&w=majority",
+    JWT_SECRET: "SHOPISTHAN",
+    GOOGLE_CLIENT: "457367745860-1hmpllp5j9ntltfeco5f42tc0gbj73e0.apps.googleusercontent.com",
+    AWS_BUCKET_REGION: "Asia Pacific (Mumbai) ap-south-1",
+    AWS_ACCESS_KEY: "AKIA6I2KNVW2I5CY34NE",
+    AWS_SECRET_KEY: "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",
+    SECREAT_KEY: 'rn5xa1lImcB7bw3Of37XzMwZ',
+    KEY_ID: 'rzp_test_rJyDhjaLVByWmI',
+    FAST_2_SMS_AUTH: 'QhKL3x5kCo1M0WlrzHVPApmwasgStNvOI9fdB4RTuj2FUGYEXDqArQfEz2ZJaNdT3OunID14Ltbg5ySG',
+    FAST_2_SMS_ROT: "dlt",
+    FAST_2_SMS_SEN_ID: "SHOPIS",
+    FAST_2_SMS_MSG_OTP: "129734",
+    FAST_2_SMS_MSG_VEN_ORD: "129735",
+    FAST_2_SMS_MSG_USE_ORD: "129733",
+    CRYPTOJS_KEY: 'Shopisthan'
+}
