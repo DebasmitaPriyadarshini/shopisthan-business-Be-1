a2275b595988dce0f859f7675f96c6e34e6df0ec
rahulc2303 <rahulc2303@gmail.com>
2022-03-31 19:46:27 +0530

    update code

--- config/prod.js
+++ config/prod.js
@@ -1,18 +1,39 @@
+//env variables in production
+
+// module.exports = {
+//     MONGOURI: process.env.MONGOURI,
+//     JWT_SECRET: process.env.JWT_SECRET,
+//     GOOGLE_CLIENT: process.env.GOOGLE_CLIENT,
+//     AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+//     AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION,
+//     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+//     AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+//     SECREAT_KEY: process.env.SECREAT_KEY,
+//     KEY_ID: process.env.KEY_ID,
+//     FAST_2_SMS_AUTH: process.env.FAST_2_SMS_AUTH,
+//     FAST_2_SMS_ROT: process.env.FAST_2_SMS_ROT,
+//     FAST_2_SMS_SEN_ID: process.env.FAST_2_SMS_SEN_ID,
+//     FAST_2_SMS_MSG_OTP: process.env.FAST_2_SMS_MSG_OTP,
+//     FAST_2_SMS_MSG_VEN_ORD: process.env.FAST_2_SMS_MSG_VEN_ORD,
+//     FAST_2_SMS_MSG_USE_ORD: process.env.FAST_2_SMS_MSG_USE_ORD, 
+//     CRYPTOJS_KEY: process.env.CRYPTOJS_KEY
+// }
+
 module.exports = {
-    MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    GOOGLE_CLIENT: process.env.GOOGLE_CLIENT,
-    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-    AWS_BUCKET_REGION: process.env.AWS_BUCKET_REGION,
-    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
-    SECREAT_KEY: process.env.SECREAT_KEY,
-    KEY_ID: process.env.KEY_ID,
-    FAST_2_SMS_AUTH: process.env.FAST_2_SMS_AUTH,
-    FAST_2_SMS_ROT: process.env.FAST_2_SMS_ROT,
-    FAST_2_SMS_SEN_ID: process.env.FAST_2_SMS_SEN_ID,
-    FAST_2_SMS_MSG_OTP: process.env.FAST_2_SMS_MSG_OTP,
-    FAST_2_SMS_MSG_VEN_ORD: process.env.FAST_2_SMS_MSG_VEN_ORD,
-    FAST_2_SMS_MSG_USE_ORD: process.env.FAST_2_SMS_MSG_USE_ORD, 
-    CRYPTOJS_KEY: process.env.CRYPTOJS_KEY
+    MONGOURI: "mongodb+srv://shopisthan:shopisthan@cluster0.iwbbb.mongodb.net/New-Flow?retryWrites=true&w=majority",
+    JWT_SECRET: "SHOPISTHAN",
+    GOOGLE_CLIENT: "457367745860-1hmpllp5j9ntltfeco5f42tc0gbj73e0.apps.googleusercontent.com",
+    AWS_BUCKET_REGION: "Asia Pacific (Mumbai) ap-south-1",
+    AWS_ACCESS_KEY: "AKIA6I2KNVW2I5CY34NE",
+    AWS_SECRET_KEY: "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",
+    SECREAT_KEY: 'rn5xa1lImcB7bw3Of37XzMwZ',
+    KEY_ID: 'rzp_test_rJyDhjaLVByWmI',
+    FAST_2_SMS_AUTH: 'QhKL3x5kCo1M0WlrzHVPApmwasgStNvOI9fdB4RTuj2FUGYEXDqArQfEz2ZJaNdT3OunID14Ltbg5ySG',
+    FAST_2_SMS_ROT: "dlt",
+    FAST_2_SMS_SEN_ID: "SHOPIS",
+    FAST_2_SMS_MSG_OTP: "129734",
+    FAST_2_SMS_MSG_VEN_ORD: "129735",
+    FAST_2_SMS_MSG_USE_ORD: "129733",
+    CRYPTOJS_KEY: 'Shopisthan'
 }
+
